6b987fa32e63d7f8c8756f033728972ae5028bab
ChriKo <komanns@fh-aachen.de>
2025-02-13 21:39:19 +0100

    Added daily fig

--- dashboard/pages/05_Analysen.py
+++ dashboard/pages/05_Analysen.py
@@ -1,9 +1,9 @@
-import logging
 
 import utils
-import graphing
 
 import streamlit as st
+import pandas as pd
+import plotly.express as px
 
 
 # page layout and config
@@ -20,4 +20,42 @@ st.set_page_config(
 utils.perform_default_page_jobs()
 
 
-charging_tab = st.tabs(["Ladesäulen"])
+weekdays_map = {
+    'Monday': 1,
+    'Tuesday': 2,
+    'Wednesday': 3,
+    'Thursday': 4,
+    'Friday': 5,
+    'Saturday': 6,
+    'Sunday': 7}
+weekday_order = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+
+charging_tab = st.tabs(["Ladesäulen"])[0]
+
+with charging_tab:
+    sql = f"""
+    SELECT
+        TO_CHAR(timestamp::timestamp, 'Day') AS weekday,
+        EXTRACT(HOUR FROM timestamp::timestamp) AS hour_of_day,
+        AVG(hourly_usage_percent) AS mean_value
+    FROM
+        temporary.ladesaeulen_usage_hourly
+    GROUP BY
+        weekday, hour_of_day
+    ORDER BY
+        weekday, hour_of_day;
+    """
+    average_week = pd.read_sql(sql=sql, con=st.session_state["engine"])
+    average_week["dayofweek"] = average_week["weekday"].map(weekdays_map)
+    average_week["mean_value"] *= 100
+
+    daily_fig = px.line(
+        average_week.sort_values(["dayofweek", "hour_of_day"]),
+        x='hour_of_day',
+        y='mean_value',
+        color='weekday',
+        labels={'hour_of_day': 'Stunde des Tages', 'mean_value': 'Mittlere Auslastung in %', 'weekday': 'Wochentag'},
+        title='Mittlere Auslastung nach Stunde und Wochentag',
+        category_orders=None)
+
+    st.plotly_chart(daily_fig)
