b0ca46a0fa1d780e8b1f98738d817b3390f04b1c
ChriKo <komanns@fh-aachen.de>
2025-02-13 21:55:57 +0100

    Added yearly verlauf and most used

--- dashboard/pages/05_Analysen.py
+++ dashboard/pages/05_Analysen.py
@@ -34,16 +34,16 @@ charging_tab = st.tabs(["Ladesäulen"])[0]
 
 with charging_tab:
     sql = f"""
-    SELECT
-        TO_CHAR(timestamp::timestamp, 'Day') AS weekday,
-        EXTRACT(HOUR FROM timestamp::timestamp) AS hour_of_day,
-        AVG(hourly_usage_percent) AS mean_value
-    FROM
-        temporary.ladesaeulen_usage_hourly
-    GROUP BY
-        weekday, hour_of_day
-    ORDER BY
-        weekday, hour_of_day;
+        SELECT
+            TO_CHAR(timestamp::timestamp, 'Day') AS weekday,
+            EXTRACT(HOUR FROM timestamp::timestamp) AS hour_of_day,
+            AVG(hourly_usage_percent) AS mean_value
+        FROM
+            temporary.ladesaeulen_usage_hourly
+        GROUP BY
+            weekday, hour_of_day
+        ORDER BY
+            weekday, hour_of_day;
     """
     average_week = pd.read_sql(sql=sql, con=st.session_state["engine"])
     average_week["dayofweek"] = average_week["weekday"].map(weekdays_map)
@@ -55,7 +55,53 @@ with charging_tab:
         y='mean_value',
         color='weekday',
         labels={'hour_of_day': 'Stunde des Tages', 'mean_value': 'Mittlere Auslastung in %', 'weekday': 'Wochentag'},
-        title='Mittlere Auslastung nach Stunde und Wochentag',
-        category_orders=None)
-
+        title='Mittlere Auslastung im Wochenverlauf nach Wochentag')
     st.plotly_chart(daily_fig)
+
+    sql = f"""
+        SELECT
+            EXTRACT(DOY FROM timestamp::timestamp) AS dayofyear,
+            AVG(hourly_usage_percent) AS mean_value
+        FROM
+            temporary.ladesaeulen_usage_hourly
+        GROUP BY
+            dayofyear
+        ORDER BY 
+            dayofyear;
+    """
+    yearly_df = pd.read_sql(sql, con=st.session_state["engine"])
+    yearly_df["mean_value"] *= 100
+    yearly_fig = px.bar(
+        data_frame=yearly_df, x="dayofyear", y="mean_value",
+        labels={"dayofyear": "Tag des Jahres", "mean_value": "Mittlere Auslastung in %"},
+        title="Mittlere Auslastung - Jahresverlauf")
+    st.plotly_chart(yearly_fig)
+
+    sql = f"""
+        SELECT
+            datastream_id AS id,
+            AVG(hourly_usage_percent) AS mean_value
+        FROM
+            temporary.ladesaeulen_usage_hourly
+        GROUP BY
+            id
+        ORDER BY mean_value DESC
+        LIMIT 11;
+    """
+    top10_df = pd.read_sql(sql, con=st.session_state["engine"])
+    top10_df = pd.merge(
+        left=top10_df,
+        right=st.session_state["datastreams"],
+        on="id")
+    top10_df = pd.merge(
+        left=top10_df,
+        right=st.session_state["sensors"],
+        left_on="sensor_id",
+        right_on="id")
+    st.header("Meistgenutzte Ladesäulen")
+    l_col, r_col = st.columns(2)
+    for idx, row in top10_df.iterrows():
+        if idx + 1 < 6:
+            l_col.write(f"**{idx+1}. Platz:** {row['description']} - {row['mean_value']*100:.2f}% Auslastung")
+        else:
+            r_col.write(f"**{idx+1}. Platz:** {row['description']} - {row['mean_value']*100:.2f}% Auslastung")
